27311cbe50dc0996a67b4aec86e15257c8c04042
hututi4717 <88864171+hututi4717@users.noreply.github.com>
2021-10-28 14:19:13 +0900

    ch passwd

--- src/com/kh/toy/common/code/Config.java
+++ src/com/kh/toy/common/code/Config.java
@@ -5,7 +5,7 @@ public enum Config {
 	DOMAIN("http://localhost:9090"),
 	COMPANY_EMAIL("mailtest4717@gmail.com"),
 	SMTP_AUTHENTICATION_ID("mailtest4717@gmail.com"),
-	SMTP_AUTHENTICATION_PASSWORD("KP5063wot^^"),
+	SMTP_AUTHENTICATION_PASSWORD("Z#85f%&e]3w]/7<"),
 	UPLOAD_PATH("C:\\CODE\\before\\upload\\");
 	
 	public final String DESC;
